0cfbd9ac7a755422d1d65115d14b8160cb2cfa56
Akin Ali <akinoali88@gmail.com>
2026-02-01 15:15:36 +0000

    create initial pass at course map scatter plot

--- src/app/base_graphs.py
+++ src/app/base_graphs.py
@@ -8,22 +8,38 @@ from plotly.graph_objects import Figure
 
 def map_golf_courses(df: pd.DataFrame) -> Figure:
 
-    '''
+    """
     Docstring for map_golf_courses
     
     :param df: Description
     :type df: pd.DataFrame
     :return: Description
     :rtype: Figure
-    '''
+    """
 
-    fig = px.scatter_map(df,
-                         lat='Latitude',
-                         lon='Longitude',
-                         hover_name='Golf Course',
-                         #size = 'tbc'
-                            )
+    fig = px.scatter_map(
+        df,
+        lat='latitude',
+        lon='longitude',
+        text='course',
+        size = 'number_of_rounds',
+        color='avg_over_par',
+        )
 
-    fig.update_layout(scattermode='group')
+    fig.update_layout()
+
+    # Update trace to position text
+    fig.update_traces(
+        textposition='bottom right',
+        mode='markers+text',
+        textfont=dict(
+            size=12,
+                ),
+    )
+
+    fig.update_layout(
+        map_style="carto-voyager-nolabels",
+        scattermode='group')    # Prevent points from overlapping
+    fig.show()
 
     return fig
