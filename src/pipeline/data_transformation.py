8f1771a291fda3fbcf0683d29533f6471973541e
Akin Ali <akinoali88@gmail.com>
2026-02-01 15:14:45 +0000

    rename file

---
+++ src/pipeline/data_transformation.py
@@ -0,0 +1,55 @@
+'''
+Aggregation functions to prepare data for reporting and analysis.
+'''
+
+import pandas as pd
+
+def generate_course_summaries(golf_rounds: pd.DataFrame,
+                              golf_courses: pd.DataFrame, ) -> pd.DataFrame:
+
+    """
+    Aggregrate scoring data from golf rounds at golf course level
+    
+    Args:
+        golf_rounds (pd.DataFrame): DataFrame containing golf rounds data, 
+            validated with the GolfRounds pydantic basemodel class
+        golf_courses (pd.DataFrame): DataFrame containing golf course data, 
+            validated with the GolfCourse pydantic basemodel class
+
+        
+    Returns:
+        pd.DataFrame: Aggregated DataFrame.
+    """
+
+    # Step Aggregate golf round data at course level
+
+    course_round_summary = golf_rounds.groupby('course').agg(
+        course=('course', 'first'),
+        number_of_rounds=('course', 'count'),
+        best_score=('score', 'min'),
+        avg_score=('score', 'mean'),
+        worst_score=('score', 'max'),
+        best_over_par=('over_par', 'min'),
+        avg_over_par=('over_par', 'mean'),
+        worst_over_par=('over_par', 'max'),
+        ).reset_index(drop=True)
+
+    # Select critical golf course information only.
+    golf_course_data = golf_courses.loc[:,
+        ['course_name',
+        'course_type',
+        'par',
+        'country_code',
+        'latitude',
+        'longitude',
+        'course_index',
+        'slope_rating',
+        ]].copy()
+
+    # Merge with golf course data to course data
+    course_round_summary = course_round_summary.merge(
+        golf_course_data,
+        left_on='course',
+        right_on='course_name').drop(columns=['course_name'])
+
+    return course_round_summary
